b246d0e79bd30b809927d5eda08e92f83538c5d1
donghyeon3813 <83585035+donghyeon3813@users.noreply.github.com>
2026-01-25 22:00:51 +0900

    [level 2] Title: 피로도, Time: 2.80 ms, Memory: 84.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -0,0 +1,24 @@
+class Solution {
+    boolean[] visited = new boolean[8];
+    int maxCount = 0;
+    public int solution(int k, int[][] dungeons) {
+        int answer = -1;
+        dfs(0, dungeons, k);
+        return maxCount;
+    }
+    public void dfs(int count, int[][] dungeons, int k) {
+        
+        if(maxCount < count){
+            maxCount = count;
+        }
+        for(int i =0; i < dungeons.length; i++){
+            if(visited[i] == false && k >= dungeons[i][0]){
+                visited[i] = true;
+                dfs(count+1, dungeons, k - dungeons[i][1]);
+                visited[i] = false;
+            }
+            
+        }
+    }
+}
+//
