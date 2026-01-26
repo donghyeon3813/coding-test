c39a7eee9a10882be0f8e9e7f132deaf9dd8a053
donghyeon3813 <83585035+donghyeon3813@users.noreply.github.com>
2026-01-26 22:36:55 +0900

    [level 2] Title: 큰 수 만들기, Time: 44.45 ms, Memory: 95.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String solution(String number, int k) {
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < number.length(); i++){
+            char c = number.charAt(i);
+            
+            while(sb.length() > 0 && sb.charAt(sb.length() - 1) < c && k > 0){
+                sb.deleteCharAt(sb.length() - 1);
+                k--;
+            }
+            sb.append(c);
+        }
+        return sb.substring(0, sb.length() - k);
+    }
+}
